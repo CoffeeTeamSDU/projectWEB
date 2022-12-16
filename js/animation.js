adc50c43e769663b5095ca6141afd810215b34e8
BekaBratan <210103206@stu.sdu.edu.kz>
2022-12-16 23:33:55 +0600

    Create animation.js

---
+++ js/animation.js
@@ -0,0 +1,3 @@
+function Animals() {
+    document.getElementsByClassName(line).appendChild(<img src="img/COFFEE-PEOPLE_FT_AREA-INTERNA_CRED_TOMAS-VELEZ-2-scaled-e1654653316847.webp" alt="gdgsdf"></img>);
+}
