f9b881e4d13450269aa88b83500a07e6e72ac839
BekaBratan <210103206@stu.sdu.edu.kz>
2022-12-19 03:57:29 +0600

    Coffees

--- COFFEES/js/animation.js
+++ COFFEES/js/animation.js
@@ -1,3 +1,13 @@
-function Animals() {
-    document.getElementsByClassName(line).appendChild(<img src="img/COFFEE-PEOPLE_FT_AREA-INTERNA_CRED_TOMAS-VELEZ-2-scaled-e1654653316847.webp" alt="gdgsdf"></img>);
+function Espresso() {
+    document.getElementsByClassName("right").innerHTML="<div class="name">
+    <div class="big_img" id="espresso"></div>
+    <div class="word">
+        <h1>Espresso</h1>
+        <p>cost: 50$ <br> you can just love it</p>
+    </div>
+</div>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur non minus ea ab corrupti sapiente quidem, fugit deleniti quam nobis, neque iusto excepturi ratione alias fugiat vero tempora. Vero recusandae perspiciatis cupiditate quis sit quod, quisquam quidem tempora. Atque dolores non quam sint dicta reprehenderit accusantium possimus magnam mollitia sed voluptatem ratione earum dolorum cum minima officia consectetur rem nam, alias suscipit! Cum cumque, quidem officiis corporis beatae adipisci recusandae similique fugiat. Tempore aliquid deserunt non ipsam laboriosam ex blanditiis doloribus consequuntur unde veniam! Doloribus deleniti adipisci officiis ea, odit eligendi ipsam enim nam obcaecati voluptatum repellat incidunt quos eius.</p>
+";
 }
+
