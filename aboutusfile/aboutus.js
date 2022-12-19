04598f93d78974d10dccac8559eaed4d907cd189
alibekmr <210103330@stu.sdu.edu.kz>
2022-12-19 16:08:07 +0600

    about6

--- aboutusfile/aboutus.js
+++ aboutusfile/aboutus.js
@@ -1,30 +1,19 @@
 function change(element){
     var a = element.innerHTML;
     var x = document.getElementById("myBR");
-    switch(a){
-        case "Alibek":
-            $("#img").css({opacity: 0, left: '25px'});
-            $("#img").animate({left:'-=25px',opacity:0},"fast");
-            document.getElementById("img").src="cat_01.jpg";
-            $("#img").animate({left:'+=25px',opacity:1});
-            break;
-        case "Bekasyl":
-            $("#img").css({opacity: 0, left: '25px'});
-            $("#img").animate({left:'-=25px',opacity:0},"fast");
-            document.getElementById("img").src="cat1.jpg";
-            $("#img").animate({left:'+=25px',opacity:1});
-            break;
-        case "Daniyar":
-            $("#img").css({opacity: 0, left: '25px'});
-            $("#img").animate({left:'-=25px',opacity:0},"fast");
-            document.getElementById("img").src="cat2.jpg";
-            $("#img").animate({left:'+=25px',opacity:1});
-            break;
-    }
+        $(".p1").animate({left:'+=50px'}, "fast");
+        $(".p1").animate({left:'-=50px'}, "fast");
+        $(".p2").animate({left:'-=50px'}, "fast");
+        $(".p2").animate({left:'+=50px'}, "fast");
+        $(".p3").animate({left:'+=50px'}, "fast");
+        $(".p3").animate({left:'-=50px'}, "fast");
+        $(".p4").animate({left:'-=50px'}, "fast");
+        $(".p4").animate({left:'+=50px'}, "fast");
+        $(".p5").animate({left:'+=50px'}, "fast");
+        $(".p5").animate({left:'-=50px'}, "fast");
     
 }
 
             function load(){
-                $(".text").animate({opacity: '1'}, "slow")
+                $(".img").animate({opacity: '1'}, "slow")
             }
-        
