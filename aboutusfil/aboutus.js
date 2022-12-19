f4535cedd3c31d3f18266423edbe3294df8a0947
alibekmr <210103330@stu.sdu.edu.kz>
2022-12-20 01:12:25 +0600

    update

--- aboutusfil/aboutus.js
+++ aboutusfil/aboutus.js
@@ -15,6 +15,15 @@ function change(element){
 
 }
 
-            function load(){
-                $(".img").animate({opacity: '1'}, "slow")
-            }
+function load(){
+        $(".img").animate({opacity: '1'}, "slow")
+}
+
+window.addEventListener('scroll', handleScroll);
+function handleScroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;  
+    var scrolled = (winScroll / height) * 100;
+
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
