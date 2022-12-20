fbcfa67fc78dcd5951b800859fabeb44a124cbce
BekaBratan <210103206@stu.sdu.edu.kz>
2022-12-20 14:06:18 +0600

    Night mode

---
+++ HOME_PAGE/js/home_page.js
@@ -0,0 +1,15 @@
+var elem = document.getElementById("btnback");
+btnback.onclick = function myFunction() {
+    if(document.body.style.backgroundColor) document.body.style.backgroundColor = "";
+    
+    else
+       document.body.style.backgroundColor = "#fff";     
+}
+
+var elem2 = document.getElementById("btnback2");
+btnback2.onclick = function myFunction() {
+    if(document.body.style.backgroundColor) document.body.style.backgroundColor = "";
+    
+    else
+       document.body.style.backgroundColor = "#fff";     
+}
--- aboutusfil/aboutus.js
+++ aboutusfil/aboutus.js
@@ -29,9 +29,17 @@ function handleScroll() {
 }
 
 var elem = document.getElementById("btnback");
-      btnback.onclick = function myFunction() {
-        if(document.body.style.backgroundColor) document.body.style.backgroundColor = "";
-        
-        else
-           document.body.style.backgroundColor = "#fff";     
-      }
+btnback.onclick = function myFunction() {
+    if(document.body.style.backgroundColor) document.body.style.backgroundColor = "";
+    
+    else
+       document.body.style.backgroundColor = "#fff";     
+}
+
+var elem2 = document.getElementById("btnback2");
+btnback2.onclick = function myFunction() {
+    if(document.body.style.backgroundColor) document.body.style.backgroundColor = "";
+    
+    else
+       document.body.style.backgroundColor = "#fff";     
+}
